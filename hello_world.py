2a49062484bb154849b52d40430011c2b7835d61
Joe DeGrand <33506282+jdegrand@users.noreply.github.com>
2019-02-23 18:28:30 -0500

    Removed excess
    Removed excess

--- hello_world.py
+++ hello_world.py
@@ -13,22 +13,10 @@ WIN_WIDTH = 800
 BOARD_WIDTH = 600
 BOARD_HEIGHT = 600
 
-current_path = os.path.dirname(__file__) # Where your .py file is located
-resource_path = os.path.join(current_path, 'pictures') # The resource folder path
-bs_path = os.path.join(resource_path, 'black_sheep.jpg')
-bs = pygame.image.load(bs_path)
-bs = pygame.transform.scale(bs, (100, 100))
-
-illum_path = os.path.join(resource_path, 'illuminated.png') # The image folder path
-illum = pygame.image.load(illum_path)
-illum = pygame.transform.scale(illum, (200, 200))
-
 win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.mixer.init()
 pygame.mixer.music.load("royalty-free-jazz.mp3")
 pygame.mixer.music.play()
-
-
 
 pygame.display.set_caption("Jazzy Sheep Betrayal")
 
@@ -156,14 +144,6 @@ def game():
             blackSheep.y -= 50
         if lastKey == "d":
             blackSheep.y += 50
-        filter = pygame.surface.Surface((BOARD_WIDTH, BOARD_HEIGHT))
-
-        filter.fill(pygame.color.Color('white'))
-        filter.blit(illum, (blackSheep.x-100, blackSheep.y-100))
-        pygame.display.flip()
-        win.blit(filter, (0, 0), special_flags=pygame.BLEND_RGBA_SUB)
-        pygame.draw.circle(win, blackSheep.color, (blackSheep.x, blackSheep.y), blackSheep.w)
-        pygame.display.update()
 
 
 if __name__ == "__main__":
