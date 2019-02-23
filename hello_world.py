e417e069d8eb52b4721b2a4a0c87424a12d1999d
Joe DeGrand <degrandjoe@gmail.com>
2019-02-23 17:37:52 -0500

    Pictures

--- hello_world.py
+++ hello_world.py
@@ -27,8 +27,11 @@ class Sheep:
     def __init__(self, black, x, y):
         if black:
             self.color = (0, 0, 0)
+            self.image = pygame.image.load('black.png')
         else:
             self.color = (255, 255, 255)
+            self.image = pygame.image.load('white.png')
+
         self.x = x
         self.y = y
         self.w = 25
@@ -41,36 +44,43 @@ def draw_grid(window):
     maxx = 1000
     maxy = 1000
     for i in range(0, maxx, 50):
-        pygame.draw.line(window,(138, 138,138), [i, 1000], [i, 0], 2)
+        pygame.draw.line(window, (138, 138, 138), [i, 1000], [i, 0], 2)
     for i in range(0, maxy, 50):
         pygame.draw.line(window, (138, 138, 138), [1000, i], [0, i], 2)
     pygame.display.update()
 
 def game():
     global q
-    draw_grid(win)
     while not q:
         pygame.time.delay(100)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                q = True
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                blackSheep.x -= 25
-                blackSheep.orient = Orientation.LEFT
-            if keys[pygame.K_RIGHT]:
-                blackSheep.x += 25
-                blackSheep.orient = Orientation.RIGHT
-            if keys[pygame.K_UP]:
-                blackSheep.y -= 25
-                blackSheep.orient = Orientation.UP
-            if keys[pygame.K_DOWN]:
-                blackSheep.y += 25
-                blackSheep.orient = Orientation.DOWN
-
-        pygame.draw.circle(win, blackSheep.color, (blackSheep.x, blackSheep.y), blackSheep.w)
+        win.fill((0, 0, 0))
+        draw_grid(win)
+        win.blit(blackSheep.image, (2 + blackSheep.x, 10 + blackSheep.y))
         pygame.display.update()
+        lastKey = None
+        timeUp = False
+        while not timeUp:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    q = True
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_LEFT]:
+                    lastKey = "l"
+                if keys[pygame.K_RIGHT]:
+                    lastKey = "r"
+                if keys[pygame.K_UP]:
+                    lastKey = "u"
+                if keys[pygame.K_DOWN]:
+                    lastKey = "d"
+
+        if lastKey == "l":
+            blackSheep.x -= 50
+        if lastKey == "r":
+            blackSheep.x += 50
+        if lastKey == "u":
+            blackSheep.y -= 50
+        if lastKey == "d":
+            blackSheep.y += 50
 
 
 #pygame.quit()
