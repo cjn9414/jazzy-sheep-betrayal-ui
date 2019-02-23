dbf16f852142517e45347b8ae25e4c26440e2b08
Carter Nesbitt <ccnesbitt@gmail.com>
2019-02-23 17:07:10 -0500

    added illumination.

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 from enum import Enum
 
 pygame.init()
@@ -8,6 +9,16 @@ WIN_WIDTH = 800
 
 BOARD_WIDTH = 600
 BOARD_HEIGHT = 600
+
+current_path = os.path.dirname(__file__) # Where your .py file is located
+resource_path = os.path.join(current_path, 'pictures') # The resource folder path
+bs_path = os.path.join(resource_path, 'black_sheep.jpg')
+bs = pygame.image.load(bs_path)
+bs = pygame.transform.scale(bs, (100, 100))
+
+illum_path = os.path.join(resource_path, 'illuminated.png') # The image folder path
+illum = pygame.image.load(illum_path)
+illum = pygame.transform.scale(illum, (200, 200))
 
 win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
@@ -21,6 +32,16 @@ class Orientation(Enum):
     RIGHT = 1
     UP = 2
     DOWN = 3
+
+
+def draw_grid(window):
+    maxx = 1000
+    maxy = 1000
+    for i in range(0, maxx, 50):
+        pygame.draw.line(window,(138, 138,138), [i, 1000], [i, 0], 2)
+    for i in range(0, maxy, 50):
+        pygame.draw.line(window, (138, 138, 138), [1000, i], [0, i], 2)
+    pygame.display.update()
 
 
 class Sheep:
@@ -41,10 +62,8 @@ blackSheep = Sheep(True, 400, 400)
 
 def game():
     global q
+    draw_grid(win)
     while not q:
-        pygame.draw.rect(win, (255, 255, 255), (200, 200, 400, 400))
-        pygame.time.delay(100)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 q = True
@@ -62,11 +81,17 @@ def game():
                 blackSheep.y += 25
                 blackSheep.orient = Orientation.DOWN
 
+        pygame.draw.rect(win, (255, 255, 255), (200, 200, 400, 400))
+        pygame.time.delay(100)
+
+        filter = pygame.surface.Surface((BOARD_WIDTH, BOARD_HEIGHT))
+
+        filter.fill(pygame.color.Color('white'))
+        filter.blit(illum, (blackSheep.x-100, blackSheep.y-100))
+        pygame.display.flip()
+        win.blit(filter, (0, 0), special_flags=pygame.BLEND_RGBA_SUB)
         pygame.draw.circle(win, blackSheep.color, (blackSheep.x, blackSheep.y), blackSheep.w)
         pygame.display.update()
-
-
-#pygame.quit()
 
 
 if __name__ == "__main__":
